b40bab2da8e8f5a545d99d76f29a289ba0840abd
michele <michele.nottoli@gmail.com>
2019-06-26 15:11:57 +0200

    preconditioner done, now working on LX.

--- newschwarz.f90
+++ newschwarz.f90
@@ -29,9 +29,66 @@ subroutine nddcosmo(phi,psi,esolv)
   return
 end subroutine nddcosmo 
 
-subroutine nlx()
+subroutine nlx(n,x,y)
   ! perform new LX multiplication
+  implicit none
+  integer, intent(in) :: n
+  real*8, intent(in) :: x(nylm,nsph)
+  real*8, intent(inout) :: y(nylm,nsph)
+  real*8, allocatable :: pot(:), basloc(:), dbasloc(:,:)
+  real*8, allocatable :: vplm(:), vcos(:), vsin(:)
+  integer :: isph, jsph, its, l1, m1, ind
+  integer :: istatus
+
+  allocate(pot(ngrid),vplm(nylm),basloc(nylm),dbasloc(3,nylm), &
+    & vcos(lmax+1),vsin(lmax+1),stat=istatus)
+  if (istatus.ne.0) then
+    write(6,*) 'Allocation failed in nlx.'
+    stop
+  end if
+
+  do isph = 1, nsph
+    call calcnv(isph,pot,x,basloc,dbasloc,vplm,vcos,vsin)
+    call intrhs(isph,pot,y(:,isph))
+  end do
+
+  deallocate(pot,basloc,dbasloc,vplm,vcos,vsin)
+  return 
 end subroutine nlx
+
+subroutine calcnv(isph,pot,sigma,basloc,dbasloc,vplm,vcos,vsin)
+  implicit none
+  integer, intent(in) :: isph
+  real*8, intent(in) :: sigma(nylm,nsph)
+  real*8, intent(out) :: pot(ngrid)
+  real*8, intent(inout) :: basloc(nylm), dbasloc(3,nylm), vplm(nylm), &
+    & vcos(lmax + 1), vsin(lmax + 1)
+  integer :: its, jsph, ij
+  real*8 :: fac1, fac2, fac3, wij, vvij, tij
+  real*8 :: vij(3), sij(3)
+
+  pot = zero
+  do its = 1, ngrid
+    if (ui(its,isph).lt.one) then
+      do ij = inl(isph),inl(isph+1)-1
+        jsph = nl(ij)
+        ! compute geometrical variables
+        vij = csph(:,isph) + rsph(isph)*grid(:,its) - csph(:,jsph)
+        vvij = sqrt(dot_product(vij,vij))
+        tij = vvij/rsph(jsph)
+        if (tij.lt.one) then
+          sij = vij/vvij
+          ! compute ddcosmo wij
+          wij = fsw(tij,se,eta)
+          if (fi(its,isph).gt.one) then
+            wij = wij/fi(its,isph)
+          end if 
+          ! assemble the rhs contraction
+        end if
+      end do
+    end if
+  end do
+end subroutine calcnv
 
 subroutine apply_nlprec()
   ! apply preconditioner
@@ -41,44 +98,48 @@ subroutine build_nlprec()
   ! build preconditioner
   implicit none
   integer :: isph, its, l1, m1, ind, lm
-  real*8 :: fac1, fac2
+  real*8 :: fac1, fac2, fac3
   integer :: istatus
   real*8, allocatable :: nlprec_bk(:,:,:), res(:,:)
   integer, allocatable :: ipiv(:)
   real*8, allocatable :: work(:)
 
   ! initialize the preconditioner
-  allocate(nlprec(nylm,nylm,nsph))
-  ! debug only
-  allocate(nlprec_bk(nylm,nylm,nsph),res(nylm,nylm))
+  allocate(nlprec(nylm,nylm,nsph),stat=istatus)
   nlprec = zero
 
-  ! allocate stuff for lapack matrix inversion
-  allocate(ipiv(nylm),work(nylm))
+  ! debug for matrix inversion
+  ! allocate(nlprec_bk(nylm,nylm,nsph),res(nylm,nylm))
 
+  ! allocate stuff for lapack matrix inversion
+  allocate(ipiv(nylm),work(nylm),stat=istatus)
+
+  write(6,*) four, pi, p, two, one
   ! dense contribution 
   do isph = 1, nsph
     fac1 = four*pi/(p*rsph(isph))
     do its = 1, ngrid
-      fac1 = fac1*w(its)*(one - ui(its,isph))
+      fac2 = fac1*w(its)*(one - ui(its,isph))
+      !write(6,*) fac2, fac1, w(its), ui(its,isph)
       do l1 = 0, lmax
         ind = l1*l1 + l1 + 1
         do m1 = -l1, l1
-          fac1 = fac1*basis(ind+m1,its)*dble(l1)/(two*dble(l1) + one)
+          fac3 = fac2*basis(ind+m1,its)*dble(l1)/(two*dble(l1) + one)
+          !write(6,*) fac3, fac2, basis(ind+m1,its), dble(l1)/(two*dble(l1)+one)
           do lm = 1, nylm
-            write(6,*) fac1*basis(lm,its)
             nlprec(lm,ind + m1,isph) = nlprec(lm,ind + m1,isph) + &
-              & fac1*basis(lm,its)
+              & fac3*basis(lm,its)
           end do 
         end do
       end do
     end do
   end do
-
+  
   ! diagonal contribution
   do isph = 1, nsph
-    do l1 = 1, lmax
+    do l1 = 0, lmax
       fac1 = four*pi/(two*dble(l1) + one)
+      ind = l1*l1 + l1 + 1
       do m1 = -l1, l1
         nlprec(ind + m1,ind + m1,isph) = nlprec(ind + m1,ind + m1,isph) &
           & + fac1
@@ -86,10 +147,12 @@ subroutine build_nlprec()
     end do 
   end do
 
+  write(8,*) '#', nylm, 1
+  call printmatrix(8,nlprec(:,:,1),nylm,nylm)
+
   ! invert 
   nlprec_bk = nlprec
   do isph = 1, nsph
-    call printmatrix(8,nlprec(:,:,isph),nylm,nylm)
     call dgetrf(nylm,nylm,nlprec(:,:,isph),nylm,ipiv,istatus)
     if (istatus.ne.0) then
       write(6,*) 'LU failed with code', istatus
@@ -103,13 +166,12 @@ subroutine build_nlprec()
   end do
 
   ! debug
-  do isph = 1, nsph
-    call dgemm('n','n',nylm,nylm,nylm,one,nlprec(:,:,isph),nylm, &
-      & nlprec_bk(:,:,isph),nylm,zero,res,nylm)
-    do lm = 1, nylm
-      write(6,*) res(lm,lm)
-    end do
-  end do
+  ! do isph = 1, nsph
+  !   call dgemm('n','n',nylm,nylm,nylm,one,nlprec(:,:,isph),nylm, &
+  !     & nlprec_bk(:,:,isph),nylm,zero,res,nylm)
+  ! end do
+
+  deallocate(ipiv,work)
   return
 end subroutine build_nlprec
 
